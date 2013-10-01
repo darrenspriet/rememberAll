d55b6d139e82b62be34339d8b39ada21350204e5
yawhide <ert.mcscrad@gmail.com>
2013-10-01 16:14:14 -0400

    ma_update

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
  * Module dependencies.
  */
 
- var express = require('express');
- var routes = require('./routes');
- var user = require('./routes/user');
- var http = require('http');
- var path = require('path');
- var User = require('./models/User.js');
- var fs = require('fs');
- var $ = require('jquery');
- var _ = require('underscore');
+ var express = require('express')
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path')
+    , User = require('./models/User.js')
+    , fs = require('fs')
+    , $ = require('jquery')
+    , _ = require('underscore');
 
  var app = express();
 
@@ -26,20 +26,15 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+var error2 = false;
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-var error2 = false;
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/form', function(req, res) {
-    if(error2){
-          res.redirect('/'); 
-          error2= false;     
-    }
-    else{
     fs.readFile('./form.html', function(error, content) {
         if (error) {
             res.writeHead(500);
@@ -47,12 +42,17 @@ app.get('/form', function(req, res) {
         }
         else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
-
-            res.end(content, 'utf-8');
-
+            if(error2){
+                res.end('<div>Though shalt only use 1-3 characters</div>' + content, 'utf-8');
+                error2 = false;
+            }
+            else{
+                res.end(content, 'utf-8');
+            }
+            
         }
     });
-    }
+    
 });
 
 
@@ -62,7 +62,7 @@ app.post('/signup', function(req, res) {
     var highscore = req.body.highscore;
     User.addUser(username, highscore, function(err, user) {
         if (err) {
-            error2 = true;
+           error2 = true;
         }
         
         res.redirect('/form');
